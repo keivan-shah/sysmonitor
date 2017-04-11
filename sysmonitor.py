800a13351e161c3e5d1f370cfd50a6b84640ad35
keivan-shah <keivanshahh@gmail.com>
2017-04-11 20:05:52 +0530

    Added Memory NotePage

--- sysmonitor.py
+++ sysmonitor.py
@@ -14,9 +14,11 @@ class SystemMonitor():
 		#NoteBook
 		self.n = Notebook(self.root)
 		self.f1 = Frame(self.n)   # CPU Usage
-		self.f2 = Frame(self.n)   # Sensors
+		self.f2 = Frame(self.n)   # Memory
+		self.f3 = Frame(self.n)   # Sensors
 		self.n.add(self.f1, text='CPU Usage')
-		self.n.add(self.f2, text='CPU Temperature')
+		self.n.add(self.f2, text='Memory')
+		self.n.add(self.f3, text='CPU Temperature')
 		self.n.pack()
 
 					######################### CPU USAGE ############################
@@ -34,6 +36,35 @@ class SystemMonitor():
 		self.label_perc.pack()
 		self.per = Label(self.f1, textvariable = self.cpu_percent )
 		self.per.pack()
+
+					#########################  MEMORY  ###########################
+		self.ram = pc.virtual_memory()
+		self.swap = pc.swap_memory()
+		self.disk = pc.disk_usage('/')
+		self.ram_percent = self.ram[2]
+		self.swap_percent = self.swap[3]
+		self.disk_percent = self.disk[3]
+
+		self.memory_window = Panedwindow(self.f2, orient=VERTICAL)
+		self.memory_frame1 = Labelframe(self.memory_window, text='RAM', width=100, height=100)
+		self.memory_frame2 = Labelframe(self.memory_window, text='Swap', width=100, height=100)
+		self.memory_frame3 = Labelframe(self.memory_window, text='Disk', width=100, height=100)
+		self.memory_window.add(self.memory_frame1)
+		self.memory_window.add(self.memory_frame2)
+		self.memory_window.add(self.memory_frame3)	
+
+		self.ram_bar = Progressbar(self.memory_frame1, length=300, value = self.ram_percent, mode="determinate")
+		self.ram_bar.pack(padx=5, pady=5)
+		self.swap_bar = Progressbar(self.memory_frame2, length=300, value = self.swap_percent, mode="determinate")
+		self.swap_bar.pack(padx=5, pady=5)
+		self.disk_bar = Progressbar(self.memory_frame3, length=300, value = self.disk_percent, mode="determinate")
+		self.disk_bar.pack(padx=5, pady=5)
+
+		self.memory_frame1.pack()
+		self.memory_frame2.pack()
+		self.memory_frame3.pack()
+		self.memory_window.pack()	
+
 
 					######################### SENSORS ############################
 		#Current Sensor Values
@@ -55,7 +86,7 @@ class SystemMonitor():
 		self.temp_cur = self.temperature['coretemp'][0][1]
 		self.temp_critical = self.temperature['coretemp'][0][3]
 
-		self.sensor_window = Panedwindow(self.f2, orient=VERTICAL)
+		self.sensor_window = Panedwindow(self.f3, orient=VERTICAL)
 		self.sensor_frame1 = Labelframe(self.sensor_window, text='Temperature', width=100, height=100)
 		self.sensor_frame2 = Labelframe(self.sensor_window, text='Battery', width=100, height=100)
 		self.sensor_window.add(self.sensor_frame1)
@@ -66,10 +97,10 @@ class SystemMonitor():
 		self.temp_bar.pack(padx=5, pady=5)
 
 		self.cur_temp = StringVar()
-		self.cur_temp.set("cur_temp: "+str(self.temp_cur))
+		self.cur_temp.set("cur_temp: "+str(self.temp_cur)+"`C")
 		self.cur_temp_label = Label(self.sensor_frame1, textvariable = self.cur_temp )
 		self.cur_temp_label.pack(side = LEFT)
-		self.label_temp = Label(self.sensor_frame1, text = "critical_temp: "+str(self.temp_critical) )
+		self.label_temp = Label(self.sensor_frame1, text = "critical_temp: "+str(self.temp_critical)+"`C" )
 		self.label_temp.pack(side = RIGHT)
 
 		#Battery Status
@@ -80,14 +111,16 @@ class SystemMonitor():
 		self.label_batt_type = Label(self.sensor_frame2, text = "Power Source: "+self.power_source )
 		self.label_batt_type.pack(side = LEFT)
 
-
 		self.sensor_frame1.pack()
 		self.sensor_frame2.pack(side = LEFT)
 		self.sensor_window.pack()
 
+					######################### UPDATE ############################		
+
 		#Call Update() after 500ms
 		self.f1.after(500, self.update_cpu)
-		self.f2.after(0,self.update_sensors)
+		self.f2.after(1000,self.update_memory)
+		self.f3.after(0,self.update_sensors)
 		#The Main Loop
 		self.root.mainloop()
 
@@ -105,7 +138,7 @@ class SystemMonitor():
 
 		self.temp_cur = self.temperature['coretemp'][0][1]
 		self.temp_bar["value"] = self.temp_cur
-		self.cur_temp.set("cur_temp: "+str(self.temp_cur))
+		self.cur_temp.set("cur_temp: "+str(self.temp_cur)+"`C")
 
 		try:
 			f = open(battery_directory+"charge_now","r")
@@ -123,7 +156,17 @@ class SystemMonitor():
 			self.batt_estimate.set("Battery Estimate: "+str(self.battery_estimate))
 		except Exception as e:
 			print (e)
-		self.f2.after(5000,self.update_sensors)
+		self.f3.after(5000,self.update_sensors)
+
+	def update_memory(self):
+		self.ram = pc.virtual_memory()
+		self.swap = pc.swap_memory()
+		self.disk = pc.disk_usage('/')
+		self.ram_percent = self.ram[2]
+		self.swap_percent = self.swap[3]
+		self.disk_percent = self.disk[3]
+
+		self.f2.after(1000,self.update_memory)
 
 
 test = SystemMonitor()
