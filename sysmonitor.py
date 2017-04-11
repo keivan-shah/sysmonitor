bff91f61f61f578fc2d9bea61779dfe6e21f8f7d
keivan-shah <keivanshahh@gmail.com>
2017-04-12 00:27:46 +0530

    Graph Somewhat Working

--- sysmonitor.py
+++ sysmonitor.py
@@ -2,6 +2,13 @@ import psutil as pc
 from tkinter import *
 from tkinter.ttk import *
 import power
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from collections import deque
 
 battery_directory = "/sys/class/power_supply/BAT1/"
 
@@ -16,14 +23,18 @@ class SystemMonitor():
 		self.f1 = Frame(self.n)   # CPU Usage
 		self.f2 = Frame(self.n)   # Memory
 		self.f3 = Frame(self.n)   # Sensors
+		self.f4 = Frame(self.n)   # Graph
 		self.n.add(self.f1, text='CPU Usage')
 		self.n.add(self.f2, text='Memory')
 		self.n.add(self.f3, text='CPU Temperature')
+		self.n.add(self.f4, text='Graph')
 		self.n.pack()
 
 					######################### CPU USAGE ############################
 		#Current CPU usage
 		self.cur_cpu = pc.cpu_percent()
+		self.cpu_arr = deque()
+		self.cpu_arr.append(self.cur_cpu)
 
 		#Progress Bar to show current CPU usage
 		self.cpu_bar = Progressbar(self.f1, length=300, value = self.cur_cpu, mode="determinate")
@@ -40,7 +51,7 @@ class SystemMonitor():
 					#########################  MEMORY  ###########################
 		self.ram = pc.virtual_memory()
 		self.swap = pc.swap_memory()
-		self.disk = pc.disk_usage('/')
+		self.disk = pc.disk_usage('/home')
 		self.ram_percent = self.ram[2]
 		self.swap_percent = self.swap[3]
 		self.disk_percent = self.disk[3]
@@ -114,7 +125,7 @@ class SystemMonitor():
 		self.cur_temp.set("cur_temp: "+str(self.temp_cur)+"`C")
 		self.cur_temp_label = Label(self.sensor_frame1, textvariable = self.cur_temp )
 		self.cur_temp_label.pack(side = LEFT)
-		self.label_temp = Label(self.sensor_frame1, text = "critical_temp: "+str(self.temp_critical)+"`C" )
+		self.label_temp = Label(self.sensor_frame1, text = "critical_temp: "+str(self.temp_critical)+"\u2103" )
 		self.label_temp.pack(side = RIGHT)
 
 		#Battery Status
@@ -129,35 +140,52 @@ class SystemMonitor():
 		self.sensor_frame2.pack(side = LEFT)
 		self.sensor_window.pack()
 
+					######################### GRAPH ############################
+		self.fig = plt.figure()
+		self.canvas = FigureCanvasTkAgg(self.fig, master=self.f4)
+		self.canvas.get_tk_widget().pack()
+
+
 					######################### UPDATE ############################		
 
 		#Call Update() after 500ms
-		self.f1.after(500, self.update_cpu)
-		self.f2.after(1000,self.update_memory)
-		self.f3.after(0,self.update_sensors)
+		self.job1 = self.f1.after(500, self.update_cpu)
+		self.job2 = self.f2.after(1000,self.update_memory)
+		self.job3 = self.f3.after(100,self.update_sensors)
+		self.job4 = self.f4.after(100,self.update_graph)
 		#The Main Loop
 		self.root.mainloop()
+		print ("Killing everything")
+		self.f1.after_cancel(self.job1)
+		self.f2.after_cancel(self.job2)
+		self.f3.after_cancel(self.job3)
+		self.f4.after_cancel(self.job4)
+		sys.exit()
 
 	#The Update function to update the CPU Usage Values
 	def update_cpu(self):
 		self.cur_cpu = pc.cpu_percent()
+		self.cpu_arr.append(self.cur_cpu)
+		if(len(self.cpu_arr)>100):
+			self.cpu_arr.popleft()
 		self.cpu_percent.set(str(self.cur_cpu)+" %")#UPDATE CPU PERCENTAGE VARIABLE
 		self.cpu_bar["value"] = self.cur_cpu
 		# print(self.cur_cpu)
-		self.f1.after(500, self.update_cpu)
+		# print(self.cpu_arr)
+		self.job1 = self.f1.after(500, self.update_cpu)
 
 	#The Update function to update the Memory Values
 	def update_memory(self):
 		self.ram = pc.virtual_memory()
 		self.swap = pc.swap_memory()
-		self.disk = pc.disk_usage('/')
+		self.disk = pc.disk_usage('/home')
 		self.ram_percent = self.ram[2]
 		self.swap_percent = self.swap[3]
 		self.disk_percent = self.disk[3]
 		self.ram_val.set("Ram Used: "+str(self.ram_percent)+"%")
 		self.swap_val.set("Swap Used: "+str(self.swap_percent)+"%")
 		self.disk_val.set("Disk Used: "+str(self.disk_percent)+"%")
-		self.f2.after(1000,self.update_memory)
+		self.job2 = self.f2.after(1000,self.update_memory)
 
 	#The Update function to update the Sensor Values
 	def update_sensors(self):
@@ -166,7 +194,7 @@ class SystemMonitor():
 
 		self.temp_cur = self.temperature['coretemp'][0][1]
 		self.temp_bar["value"] = self.temp_cur
-		self.cur_temp.set("cur_temp: "+str(self.temp_cur)+"`C")
+		self.cur_temp.set("cur_temp: "+str(self.temp_cur)+"\u2103")
 
 		try:
 			f = open(battery_directory+"charge_now","r")
@@ -184,7 +212,20 @@ class SystemMonitor():
 			self.batt_estimate.set("Battery Estimate: "+str(self.battery_estimate))
 		except Exception as e:
 			print (e)
-		self.f3.after(3000,self.update_sensors)
+		self.job3 = self.f3.after(3000,self.update_sensors)
+
+	def update_graph(self):
+		v = np.array(self.cpu_arr)
+		x = np.arange(0,len(v),1)
+		# print (len(x),len(v))
+		plot(x,v)
+		self.job4 = self.f4.after(500, self.update_graph)
+
+def plot(x, y):
+	plt.clf()
+	plt.plot(x,y)
+	plt.axis([0,100,0,100])
+	plt.gcf().canvas.draw()
 
 
 test = SystemMonitor()
